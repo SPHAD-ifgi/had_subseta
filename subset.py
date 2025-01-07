23cb49addde7f40e7ebd02d614b432a196139e4d
niebl <caro.niebl@uni-muenster.de>
2025-01-07 17:59:47 +0100

    optimize tile containment check

--- subset.py
+++ subset.py
@@ -10,20 +10,24 @@ def process_csv(reader, bbox):
     #Process CSV data row by row within the given bounding box.
     xmin, ymin, xmax, ymax = bbox if bbox else (None, None, None, None)
 
-    # there is probably a quicker way to do this by considering containing quads
-    tiles = mercantile.tiles(xmin, ymin, xmax, ymax, 18)
-    quadkeys = [mercantile.quadkey(tile) for tile in tiles]
 
-    print("AGG_DAY_PERIOD,ACTIVITY_INDEX_TOTAL,GEOGRAPHY")
     for row in reader:
-        try:
-            #filter only tiles within bounding box
-            #use mercantile for that
-            if(row[2] in quadkeys):
-                print(row[0] + "," + row[1] + "," + row[2])
-        except ValueError:
-            # Handle or skip rows with invalid data
-            continue
+        if(row[2] == "GEOGRAPHY"):
+            print("AGG_DAY_PERIOD,ACTIVITY_INDEX_TOTAL,GEOGRAPHY")
+        else:
+            try:
+                tile = mercantile.quadkey_to_tile(row[2])
+                tile = mercantile.bounds(tile)
+
+                contained = tile.west >= xmin and tile.east <= xmax
+                contained = tile.south >= ymin and tile.north <= ymax and contained
+
+                #use mercantile for that
+                if(contained):
+                    print(row[0] + "," + row[1] + "," + row[2])
+            except ValueError:
+                # Handle or skip rows with invalid data
+                continue
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="filter HAD CSV within specified bounding box. \n This just assumes the following file structure: col 1: date, col 2: HAD index, col3: quadkey index")
